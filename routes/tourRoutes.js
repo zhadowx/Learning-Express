43d82bf635f6f243d4baf538b795d69455f8ff4b
Nelson Lora <nlorax@gmail.com>
2024-05-31 11:29:20 -0400

    Refactor API features and utilize aggregation pipeline operators

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,10 +6,13 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController');
 
 const router = express.Router();
-
+router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
