b992c1def75523faa519de634a5186be69a66d79
Nelson Lora <nlorax@gmail.com>
2024-06-06 14:11:26 -0400

    Add users model, integrate jwt authentication and protect tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,12 +9,13 @@ const {
   getTourStats,
   getMonthlyPlan,
 } = require('../controllers/tourController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(authController.protect, getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,11 +5,16 @@ const {
   getUser,
   updateUser,
   deleteUser,
-} = require('./../controllers/userController');
+} = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
 router.route('/').get(getAllUsers).post(createUser);
+
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
